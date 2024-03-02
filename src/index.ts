865205527933c2928130b716b847e5d8872ba31a
roser.dev <>
2024-03-02 21:13:49 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+// We define the empty imports so the auto-complete feature works as expected.
+import { } from '@mtvproject/sdk/math'
+import { Animator, AudioSource, AvatarAttach, GltfContainer, Material, Transform, UiBackground, UiText, UiTransform, VideoPlayer, VisibilityComponent, engine, pointerEventsSystem } from '@mtvproject/sdk/ecs'
+import { initAssetPacks } from '@mtvproject/asset-packs/dist/scene-entrypoint'
+
+import { changeColorSystem, circularSystem } from './systems'
+import { setupUi } from './ui'
+
+// You can remove this if you don't use any asset packs
+initAssetPacks(engine, pointerEventsSystem, {
+  Animator,
+  AudioSource,
+  AvatarAttach,
+  Transform,
+  VisibilityComponent,
+  GltfContainer,
+  Material,
+  VideoPlayer,
+  UiTransform,
+  UiText,
+  UiBackground
+})
+
+export function main() {
+  // Defining behavior. See `src/systems.ts` file.
+  engine.addSystem(circularSystem)
+  engine.addSystem(changeColorSystem)
+
+  // draw UI. Here is the logic to spawn cubes.
+  setupUi()
+}
